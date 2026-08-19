e216a60137708556b58d8cf9789b25a7dc455622
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-08-19 11:40:28 -0300

    Adicionando codigo da aula 4

---
+++ Aula 4/JavaApplication2/src/javaapplication2/Pessoa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Pessoa {
+    public String nome; 
+    public char sexo; 
+    public String idioma; 
+
+    public Pessoa(String nome, char sexo, String idioma) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idioma = idioma;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + " nome: " + nome + ", sexo: " + sexo + ", idioma: " + idioma;
+    }   
+}
